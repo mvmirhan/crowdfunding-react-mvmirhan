4411f3fb6e741003268ce7927b9e9daf66a628ef
mvmirhan <mvmirhan@yahoo.com>
2022-09-18 14:29:35 +1000

    Fix issues with login form

--- src/components/LoginForm/Index.jsx
+++ src/components/LoginForm/Index.jsx
@@ -12,32 +12,32 @@ const LoginForm = ({setLoggedInUser}) => {
     const navigate = useNavigate()
 
     const handleChange = (e) => {
-        const { id, value } = e.target;
-        setCredentials({...credentials,[id]: value})
-        // setCredentials((prevCredentials) => ({
-        //     ...prevCredentials,[id]: value,}))
+        e.preventDefault()
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value})
         }
-    
-
+        
     const postData = async () => {
         const response = await fetch
         (`${process.env.REACT_APP_API_URL}api-token-auth/`,
             {
                 method: "post",
-                headers: {"Content-type": "application/json",},
+                headers: 
+                {
+                    "Content-type": "application/json",
+                },
                 body: JSON.stringify(credentials),
             })
             return response.json()
     }
     
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         if(credentials.username && credentials.password) {
             postData().then((data) => {
-                window.localStorage.setItem("token", data.token)
-                console.log(data.token)
-                setLoggedInUser({id: data.id})
-                console.log(data.id)
+            console.log(data)
+                window.localStorage.setItem("token", data.token,)
                 navigate("/")
             })}
     }
@@ -47,13 +47,13 @@ const LoginForm = ({setLoggedInUser}) => {
         <div className='form_body'>
             <div>
                 <label className='form_label' htmlFor='username'>Username:</label>
-                <input className='form_input' type='text' id='username' placeholder='Enter username'
+                <input className='form_input' type='text' name='username' placeholder='Enter username'
                 onChange={handleChange}/>
             </div>
 
             <div>
                 <label className='form_label' htmlFor='password'>Password:</label>
-                <input className='form_input' type='password' id='password' placeholder='Enter password'
+                <input className='form_input' type='password' name='password' placeholder='Enter password'
                 onChange={handleChange}/>
             </div>
             <div>
